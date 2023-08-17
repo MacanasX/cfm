ee66340a6dab28cb48b0040c5e6ac0d06a34b239
Luca Groß <gross-darmstadt@web.de>
2023-08-17 12:02:33 +0200

    added databaseconfig to read env variables, added TypeORM module to connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [databaseConfig.KEY],
+      useFactory: (dbConfig: ConfigType<typeof databaseConfig>) => ({
+        ...dbConfig,
+        type: 'mysql',
+        autoLoadEntities: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/database.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config';
+import * as process from 'process';
+
+export default registerAs('database', () => ({
+  port: +(process.env.DATABASE_PORT ?? 3306),
+  host: process.env.DATABASE_HOST ?? '',
+  username: process.env.DATABASE_USERNAME ?? '',
+  password: process.env.DATABASE_PASSWORD ?? '',
+  database: process.env.DATABASE_NAME ?? '',
+  synchronice: process.env.DATABASE_SYNCHRONICE?.toLowerCase() === 'true',
+}));
